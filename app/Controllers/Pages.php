3b43236f94883f7165f742432333832eb418d64c
Ortiv Inga <ortiv@programmer.net>
2022-10-30 13:34:53 -0400

    Pages Controller

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,12 +1,14 @@
-<?php
-
+<?php 
 namespace App\Controllers;
-
-class Home extends BaseController
+class Pages extends BaseController
 {
     public function index()
     {
         return view('welcome_message');
     }
-    
+
+    public function view($page = 'home')
+    {
+        // ...
+    }
 }
---
+++ app/Views/templates/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Codeigniter Tutorial</title>
+</head>
+<body>
+    <?php //short hand for <?php echo $title ?>
+    <h1><?= esc($title) ?></h1>

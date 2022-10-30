5b7e58e372bcfc144d8ff7d7e8379df9a5f897dc
Ortiv Inga <ortiv@programmer.net>
2022-10-30 17:22:04 -0400

    feature: add news section

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,5 +1,7 @@
-<?php 
+<?php
+
 namespace App\Controllers;
+
 class Pages extends BaseController
 {
     public function index()
@@ -9,6 +11,15 @@ class Pages extends BaseController
 
     public function view($page = 'home')
     {
-        // ...
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
     }
 }
---
+++ app/Views/pages/about.php
@@ -0,0 +1,1 @@
+This is Pages about.php<br />
